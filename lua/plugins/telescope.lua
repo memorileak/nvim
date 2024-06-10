209d0b20507fe9bcbe3c113f2cc5fe6db2d641e4
Tung Nguyen <tungnvan.mar12th@gmail.com>
2024-06-10 10:13:54 +0700

    Added feature: telescope find selected word

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,24 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+local opts = { noremap = true, silent = true }
+
+vim.keymap.set('n', '<leader>ff', builtin.find_files, opts)
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, opts)
+vim.keymap.set('n', '<leader>fb', builtin.buffers, opts)
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, opts)
+vim.keymap.set('v', '<leader>fw', function()
+	local text = vim.getVisualSelection()
+	builtin.live_grep({ default_text = text })
+end, opts)
+
+-- local keymap = vim.keymap.set
+-- local tb = require('telescope.builtin')
+-- local opts = { noremap = true, silent = true }
+--
+-- keymap('n', '<space>g', ':Telescope current_buffer_fuzzy_find<cr>', opts)
+-- keymap('v', '<space>g', function()
+-- 	local text = vim.getVisualSelection()
+-- 	tb.current_buffer_fuzzy_find({ default_text = text })
+-- end, opts)
+--
+-- keymap('n', '<space>G', ':Telescope live_grep<cr>', opts)
 
