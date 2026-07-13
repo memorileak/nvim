941cff22a1c1aeaac2463aa33b3472c9501eddbb
Tung Nguyen <tung.nguyen@codelink.io>
2026-07-13 14:42:06 +0700

    [NoLSP] Update ESLint formatter & error format

--- ftplugin/javascript.lua
+++ ftplugin/javascript.lua
@@ -1,3 +1,9 @@
 vim.cmd("compiler eslint")
 
-vim.opt_local.makeprg = "npx --no-install eslint --format=unix ."
+local pipe_head = [[ \| head -n -1]] -- Remove the last line.
+local pipe_rg = [[ \| rg -v '^\s*$']] -- Remove empty lines.
+
+vim.opt_local.makeprg = "npx --no-install eslint -f $(npm root -g)/eslint-formatter-compact/index.js ."
+  .. pipe_head
+  .. pipe_rg
+vim.opt_local.errorformat = "%f: line %l\\, col %c\\, %t%*\\a - %m"
