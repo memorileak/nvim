7dd5f41dc51b27fe705788b329809158432293e7
Tung Nguyen <tung.nguyen@codelink.io>
2026-07-14 17:55:12 +0700

    [NoLsp] TypeScript linting using both tsc and eslint

--- ftplugin/javascriptreact.lua
+++ ftplugin/javascriptreact.lua
@@ -3,5 +3,5 @@ vim.cmd("compiler eslint")
 local config_dir = vim.fn.stdpath("config")
 local formatter_path = config_dir .. "/utils/eslint-formatter.js"
 
-vim.opt_local.makeprg = "npx --no-install eslint -f " .. formatter_path .. " ."
+vim.opt_local.makeprg = "npx --no-install eslint -f " .. vim.fn.shellescape(formatter_path) .. " ."
 vim.opt_local.errorformat = [[%f(%l\,%c): %t : %m]]
--- ftplugin/typescript.lua
+++ ftplugin/typescript.lua
@@ -1,3 +1,10 @@
 vim.cmd("compiler tsc")
 
-vim.opt_local.makeprg = "npx --no-install tsc --noEmit --pretty false"
+local formatter = vim.fn.stdpath("config") .. "/utils/eslint-formatter.js"
+
+-- Run both regardless of first command exit status
+vim.opt_local.makeprg = "sh -c 'npx --no-install tsc --noEmit --pretty false; npx --no-install eslint -f "
+  .. vim.fn.shellescape(formatter)
+  .. " .'"
+
+vim.opt_local.errorformat = [[%f(%l\,%c): %t%*[^ ] %m,%f(%l\,%c): %t : %m]]
