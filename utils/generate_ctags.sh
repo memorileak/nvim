0eea082b383559bd00c6c51d9ea6471ffb5bdb1e
Tung Nguyen <tung.nguyen@codelink.io>
2026-09-03 16:24:36 +0800

    [NoLSP] Append rust-src ctags to the generated .tags file

--- utils/generate_ctags.sh
+++ utils/generate_ctags.sh
@@ -29,7 +29,22 @@ echo "Generating tags file for ${#files[@]} files..."
 
 printf "%s\n" "${files[@]}" | ctags --quiet -L - -f .tags
 
-echo "Tags file .tags successfully generated."
+# If in the current directory there is a rusty-tags.vi file,
+# append its content to our generated .tags file
+if [ -f "rusty-tags.vi" ]; then
+  cat rusty-tags.vi >>.tags
+  # This indicates that the current directory is a Rust project.
+  # If rustc is available, get its sysroot,
+  # append with /lib/rustlib/src/rust/library/rusty-tags.vi
+  # Check if the file exists, and if so, append its content to our generated .tags file
+  if command -v rustc >/dev/null 2>&1; then
+    RUST_SYSROOT=$(rustc --print sysroot)
+    RUST_TAGS_FILE="$RUST_SYSROOT/lib/rustlib/src/rust/library/rusty-tags.vi"
+    if [ -f "$RUST_TAGS_FILE" ]; then
+      cat "$RUST_TAGS_FILE" >>.tags
+    fi
+  fi
+fi
 
 dedup() {
   if [ -f "$1" ]; then
@@ -37,11 +52,6 @@ dedup() {
   fi
 }
 
-# If in the current directory there is a rusty-tags.vi file,
-# append its content to our generated .tags file
-if [ -f "rusty-tags.vi" ]; then
-  echo "Appending rusty-tags.vi to .tags file..."
-  cat rusty-tags.vi >>.tags
-  dedup .tags
-  echo "Appended rusty-tags.vi to .tags file."
-fi
+dedup .tags
+
+echo "Tags file .tags successfully generated."
